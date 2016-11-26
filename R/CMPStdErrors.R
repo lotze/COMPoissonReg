9573ada86e0fd7823144d2a0fd748cc3ea3c42f0
Andrew Raim <andrew.raim@gmail.com>
2016-11-25 19:41:06 -0500

    Updated notes Added some commented out code about unifying FIM code

--- R/CMPStdErrors.R
+++ R/CMPStdErrors.R
@@ -6,6 +6,13 @@ function(x,beta,nu,max=100){
    Ibetanu <- InfoMatrix.betanu(x,beta,nu,max)
    Inu <- InfoMatrix.nu(x,beta,nu,max)
    info <- InfoMatrix(Ibeta,Ibetanu,Inu)
+
+   # If we want to put a regression on nu, we can get Std Errors like this
+   # via ZICMP FIM. [Do we want to support that regression?]
+   # qq <- ncol(X) + ncol(S)
+   # FF <- fim.zicmp.reg(X, S, W = matrix(1,1,1), beta, gamma, zeta = 0, max = max)
+   # info <- FF[1:qq, 1:qq]
+
    CMP.SEs <- sqrt(diag(solve(info)))
 
 return(CMP.SEs)
