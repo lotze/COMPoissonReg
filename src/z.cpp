bd34ef0270c63649a671e3eab9170eed7321e28b
Andrew Raim <andrew.raim@gmail.com>
2018-06-15 09:40:40 -0400

    Make sure to initialize z Add check for infinity, to make sure we stop summing

--- src/z.cpp
+++ src/z.cpp
@@ -1,6 +1,5 @@
 #include <Rcpp.h>
 
-// TBD: Code does not halt if a z(i) reaches Inf and ymax is not specified
 // [[Rcpp::export]]
 Rcpp::NumericVector computez_adapt(const Rcpp::NumericVector& lambda,
 	const Rcpp::NumericVector& nu, double tol = 1e-6, bool take_log = false,
@@ -11,13 +10,14 @@ Rcpp::NumericVector computez_adapt(const Rcpp::NumericVector& lambda,
 		Rcpp::stop("Length of lambda must be same as length of nu");
 	}
 	Rcpp::NumericVector z(n);
+	z.fill(0);
 
 	for (unsigned int i = 0; i < n; i++) {
 		double delta = R_PosInf;
 		double psi = -0.5772157;	// Euler–Mascheroni constant
 		double y = 0;
 		bool is_increase = true;
-		while ((delta > tol || is_increase) && y <= ymax) {
+		while ((delta > tol || is_increase) && y <= ymax && !isinf(z(i))) {
 			delta = exp( y*log(lambda(i)) - nu(i)*lgamma(y+1) );
 			z(i) += delta;
 			psi += 1 / (y+1);
@@ -26,7 +26,7 @@ Rcpp::NumericVector computez_adapt(const Rcpp::NumericVector& lambda,
 			y++;
 		}
 	}
-	
+
 	if (take_log) {
 		return log(z);
 	} else {
