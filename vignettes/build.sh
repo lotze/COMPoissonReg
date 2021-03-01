984a45afa13fb3d2593f4a33bab292726951081a
Andrew Raim <andrew.raim@gmail.com>
2021-03-01 15:38:45 -0500

    Some updates

--- vignettes/build.sh
+++ vignettes/build.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-Rscript -e "devtools::document(roclets = 'vignette')"
+# This bash script compiles any vignettes present in the `vignettes` folder of
+# the project, relative to the current working directory.
+#
+# You can also run the lines below within R from any platform to get the same
+# effect.
+
+
+read -r -d '' SCRIPT << EOF
+require("R.rsp")
+devtools::document(roclets = "vignette")
+EOF
+
+Rscript -e "${SCRIPT}"
 
